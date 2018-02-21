d4a3a2f9b355c1059c639afa0cb84f57eed4b1b6
Nvleijden <nick.vanleijden@gmail.com>
2018-02-21 16:58:54 +0100

    musicplayer update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 var arrow = document.querySelector("#arrow");
+var playButton = document.querySelector(".play_button");
+var forwardButton = document.querySelector("#forward_button");
+var song = document.querySelector("#song");
+var genre = "hello";
+var genreBox = document.querySelector("#genre");
+var oldgenre = "niks";
+var i = 0;
+
 window.sr = ScrollReveal();
 sr.reveal('.me');
 sr.reveal('.workItem', {
-    reset: true,
+    reset: true
 });
+
 document.addEventListener("scroll", function () {
 
     if (window.pageYOffset > 50) {
@@ -23,3 +32,42 @@ document.addEventListener("scroll", function () {
 
     }
 });
+// dance music
+var dance = ["./audio/dance/Beam.mp3", "./audio/dance/IngridHybrid.mp3", "./audio/dance/Constellations.mp3"];
+
+var danceNames = ["Beam", "Ingrid Is A Hybrid", "Constellations"];
+var danceArtists = ["Mako", "Dusky", "Fred V & Grafix"];
+
+
+song.src = dance[1];
+
+
+genreBox.onchange = function () {
+    console.log("ik werk");
+    console.log(genreBox.value);
+    console.log(oldgenre);
+
+
+    oldgenre = genreBox.value;
+}
+
+function playPause() {
+    if (songActive == 0) {
+        song.play();
+
+        playButton.classList.add("play_button_clicked");
+        playButton.classList.remove("play_button");
+    } else {
+        song.pause();
+
+        playButton.classList.toggle("play_button");
+        playButton.classList.remove("play_button_clicked");
+    }
+
+
+}
+
+//select next song
+song.onended = nextSong();
+
+function nextSong() {}
