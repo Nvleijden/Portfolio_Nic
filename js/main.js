40de01a93685ba8ce07e36944853d7e606aab73e
Nvleijden <nick.vanleijden@gmail.com>
2018-02-21 18:04:37 +0100

    Revert "Musicplayer>V2"
    This reverts commit b0d12624d410673c37eb15e3461b1fbda609c67a.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-//select beam by Mako as standard song
-
 var arrow = document.querySelector("#arrow");
 var playButton = document.querySelector(".play_button");
 var forwardButton = document.querySelector("#forward_button");
-var artist = document.querySelector("#artistName");
-var title = document.querySelector("#songName");
 var song = document.querySelector("#song");
 var genre = "hello";
 var genreBox = document.querySelector("#genre");
@@ -38,12 +34,12 @@ document.addEventListener("scroll", function () {
 });
 // dance music
 var dancing = ["./audio/dance/Beam.mp3", "./audio/dance/IngridHybrid.mp3", "./audio/dance/Constellations.mp3"];
-var danceTitles = ["Beam", "Ingrid Is A Hybrid", "Constellations"];
+var danceNames = ["Beam", "Ingrid Is A Hybrid", "Constellations"];
 var danceArtists = ["Mako", "Dusky", "Fred V & Grafix"];
 
 
 
-song.src = dancing[0];
+
 
 genreBox.onchange = function () {
     if (genreBox.value != oldgenre) {
@@ -54,8 +50,6 @@ genreBox.onchange = function () {
         if (genreBox.value == "dancing") {
             console.log("hi");
             song.src = dancing[0];
-            title.innerHTML = danceTitles[i];
-            artist.innerHTML = danceArtists[i];
             playPause();
             song.play();
         }
